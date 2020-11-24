cc6460c9fc06a903eb309f38d488cc15969b165c
delims <delims@qq.com>
2020-11-24 09:25:50 +0800

    init

---
+++ 11_24_macos_cpp/11_24_macos_cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  11_24_macos_cpp
+//
+//  Created by delims on 2020/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
